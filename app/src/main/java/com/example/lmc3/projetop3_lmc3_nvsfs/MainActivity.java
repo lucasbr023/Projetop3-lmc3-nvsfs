5fd20fbbca009202250396198ea8d276c19588cf
nvsfs@cin.ufpe.br <nvsfs@cin.ufpe.br>
2017-05-27 23:30:00 -0300

    Main navbar

--- app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/MainActivity.java
+++ app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lmc3.projetop3_lmc3_nvsfs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navBar = (BottomNavigationView) findViewById(R.id.navBot);
+
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.id_weather:
+                        //todo
+                        Log.d("P3", "WEATHER");
+                        return true;
+                    case R.id.id_currency:
+                        //todo
+                        Log.d("P3", "CURRENCY");
+                        return true;
+                    case R.id.id_map:
+                        Log.d("P3", "MAP");
+                        //todo
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
     }
+
+
 }
