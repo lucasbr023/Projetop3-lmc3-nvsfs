ae93f1293d80c405cb5f9a1ff83ad3096484bf62
Lucas Carvalho <lmc3@cin.ufpe.br>
2017-06-03 11:03:16 -0300

    Create and activities (Weather and Currency)

--- app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/CurrencyActivity.java
+++ app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/CurrencyActivity.java
@@ -3,46 +3,48 @@ package com.example.lmc3.projetop3_lmc3_nvsfs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by lmc3 on 03/06/2017.
+ */
 
+public class CurrencyActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //overridePendingTransition(R.anim.pull_in_from_left, R.anim.hold);
+        setContentView(R.layout.activity_currency);
 
         BottomNavigationView navBar = (BottomNavigationView) findViewById(R.id.navBot);
         Menu menu = navBar.getMenu();
-        MenuItem menuItem = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
-
         navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.id_weather:
-                        Intent intentWeather = new Intent(MainActivity.this, WeatherActivity.class);
+                        Intent intentWeather = new Intent(CurrencyActivity.this, WeatherActivity.class);
                         intentWeather.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         startActivity(intentWeather);
                         return true;
                     case R.id.id_currency:
-                        Intent intentCurrency = new Intent(MainActivity.this, CurrencyActivity.class);
-                        intentCurrency.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(intentCurrency);
                         return true;
                     case R.id.id_map:
+                        Intent intentMap = new Intent(CurrencyActivity.this, MainActivity.class);
+                        intentMap.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intentMap);
                         return true;
                 }
                 return true;
             }
         });
     }
-
-
 }
--- app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/WeatherActivity.java
+++ app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/WeatherActivity.java
@@ -3,46 +3,47 @@ package com.example.lmc3.projetop3_lmc3_nvsfs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by lmc3 on 03/06/2017.
+ */
 
+public class WeatherActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_weather);
 
         BottomNavigationView navBar = (BottomNavigationView) findViewById(R.id.navBot);
         Menu menu = navBar.getMenu();
-        MenuItem menuItem = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(2);
         menuItem.setChecked(true);
-
         navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.id_weather:
-                        Intent intentWeather = new Intent(MainActivity.this, WeatherActivity.class);
-                        intentWeather.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(intentWeather);
                         return true;
                     case R.id.id_currency:
-                        Intent intentCurrency = new Intent(MainActivity.this, CurrencyActivity.class);
+                        Intent intentCurrency = new Intent(WeatherActivity.this, CurrencyActivity.class);
                         intentCurrency.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         startActivity(intentCurrency);
                         return true;
                     case R.id.id_map:
+                        Intent intentMap = new Intent(WeatherActivity.this, MainActivity.class);
+                        intentMap.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intentMap);
                         return true;
                 }
                 return true;
             }
         });
     }
-
-
 }
