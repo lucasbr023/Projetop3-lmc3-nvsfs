5ff6c8b088eb89fdcdf6204aa5c5d39d5cae78d4
nvsfs@cin.ufpe.br <nvsfs@cin.ufpe.br>
2017-06-11 11:22:07 -0300

    setting location and passing to the weather activity

--- app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/MainActivity.java
+++ app/src/main/java/com/example/lmc3/projetop3_lmc3_nvsfs/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.lmc3.projetop3_lmc3_nvsfs;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleMap.OnMapLongClickListener {
 
 
-public class MainActivity extends FragmentActivity {
+    private GoogleMap map;
+    LocationManager locationManager;
+
+    public Double latitude;
+    public Double longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Map
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
+
+
+        //Navbar
         BottomNavigationView navBar = (BottomNavigationView) findViewById(R.id.navBot);
         Menu menu = navBar.getMenu();
         MenuItem menuItem = menu.getItem(0);
@@ -48,4 +78,70 @@ public class MainActivity extends FragmentActivity {
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+
+        map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+
+                                             // Passing Location (lat and long) to the other activity)
+                                             @Override
+                                             public void onInfoWindowClick(Marker marker) {
+
+                                                 Intent intentBundle = new Intent(MainActivity.this, WeatherActivity.class);
+                                                 Bundle bundle = new Bundle();
+                                                 bundle.putDouble("latitude", latitude);
+                                                 bundle.putDouble("longitude", longitude);
+
+                                                 intentBundle.putExtras(bundle);
+                                                 startActivity(intentBundle);
+
+                                             }
+                                         }
+        );
+
+        map.setOnMapLongClickListener(this);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        map.setMyLocationEnabled(true);
+
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+
+        if (location != null) {
+
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()),13));
+
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 12.0f));
+            }
+    }
+
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+
+        Double l1 = latLng.latitude;
+        Double l2 = latLng.longitude;
+
+        latitude = latLng.latitude;
+        longitude = latLng.longitude;
+
+
+        map.addMarker(new MarkerOptions().position(new LatLng(l1, l2)).title("teste"));
+
+    }
 }
